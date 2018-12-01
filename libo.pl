e8fce62dea1212f5ea0c95602f3580b851004d82
Hodaka Suzuki <altitude3190@gmail.com>
2018-12-01 19:03:34 +0900

    update libo.pl

--- libo.pl
+++ libo.pl
@@ -3,30 +3,73 @@
 use strict;
 use warnings;
 use utf8;
+
 use Pod::Usage qw/pod2usage/;
 use File::Basename qw/basename/;
 
 my $VERSION = '1.0.0';
 my $LIC_TXT_DIR_PATH= './boilerplates';
 
-my ($cmd, @args) = @ARGV;
+# treat arguments
+my $author = `git config user.name` || '';
+my $cmd = '';
+my $args = [];
+while (my $arg = shift @ARGV) {
+    if ($arg eq '--author') {
+        $author = shift @ARGV;
+    } else {
+        if ($cmd) {
+            push $args, $arg;
+        } else {
+            $cmd = $arg;
+        }
+    }
+}
+
 pod2usage(verbose => 2, noperldoc => 1, exitval => 0) unless $cmd;
 
-my $CMD_MAP = +{
+my $replace_map = +{
+    yyyy => (localtime(time))[5] + 1900,
+    author => $author,
+};
+
+my $cmd_map = +{
     dump => sub {
         my $args = shift;
 
         for my $lic_name (@$args) {
-
             open my $fh, "< $LIC_TXT_DIR_PATH/$lic_name.txt" or die "no such a license: $lic_name";
 
+            my @lines = ();
             while (my $line = <$fh>) {
-                chomp $line;
-                print "$line\n";
+                while ($line =~ /s!(.*?)!(.*?)!/g) {
+                    my $original = $1;
+                    my $replace_type = $2;
+
+                    if (my $replaced_word = $replace_map->{$replace_type}) {
+                        $line =~ s/s!.*?!$replace_type!/$replaced_word/;
+                    } else {
+                        # Some licenses request information except for dates and author name.
+                        # In the case, the information is given from stdin.
+
+                        # I don't want to output this sentence to stdout
+                        # because I assume usages like 'libo dump mit > LISENCE',
+                        # so use warn.
+                        warn "hey, input $original : \n";
+                        my $stdin = <STDIN>;
+                        chomp $stdin;
+                        if ($stdin) {
+                            $line =~ s/s!\Q$original\E!$replace_type!/$stdin/;
+                        } else {
+                            $line =~ s/s!\Q$original\E!$replace_type!/$original/;
+                        }
+                    }
+                }
+                push @lines, $line;
             }
 
+            print @lines;
             close $fh;
-
             print "-" x 80, "\n" if scalar @$args > 1 && $lic_name ne $args->[-1];
         }
     },
@@ -52,7 +95,7 @@ my $CMD_MAP = +{
     },
 };
 
-unless (exists $CMD_MAP->{$cmd}) {
+unless (exists $cmd_map->{$cmd}) {
     pod2usage(
         message => "Command not found\n",
         verbose => 2,
@@ -61,23 +104,25 @@ unless (exists $CMD_MAP->{$cmd}) {
     );
 }
 
-$CMD_MAP->{$cmd}(\@args);
+$cmd_map->{$cmd}($args);
 
 __END__
 
 =head1 NAME
 
-  libo -- Easy access to license boilerplates
+  libo -- short for 'license boilerplates'
+  Enable to make license texts easily
 
 =head1 SYNOPSIS
 
   libo <command>
 
   Commands:
-      dump <name1> [name2 ...]         Write boilerplate(s)
-      list                             List available boilerplates
-      search <string1> [stirng2 ...]   Search for available boilerplates with string(s)
-      version                          Display current version of this script
-      help                             Display help of this script
+      dump <license_name1> [license_name2 ...] [--author <name>]  Write boilerplate(s)
+                                                                  --author is used for license text. If empty, git user name is used.
+      list                                                        List available boilerplates
+      search <string1> [stirng2 ...]                              Search for available boilerplates with string(s)
+      version                                                     Display current version of this script
+      help                                                        Display help of this script
 
 =cut
